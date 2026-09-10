e8b40ed25d6660a2956ac30965f93ecbdf480289
Lukasz Kielbasa <hamsingming@gmail.com>
2026-09-10 13:12:30 +0200

    chore: initial framework setup with env management and secrets

---
+++ cypress.config.js
@@ -0,0 +1,27 @@
+const { defineConfig } = require("cypress");
+const dotenvPlugin = require('cypress-dotenv');
+const fs = require('fs');
+const path = require('path');
+
+function getConfigurationByFile(env) {
+  const pathToConfigFile = path.resolve('config', `${env}.json`);
+  if (!fs.existsSync(pathToConfigFile)) {
+    throw new Error(`Config file ${pathToConfigFile} does not exist`);
+  }
+  return JSON.parse(fs.readFileSync(pathToConfigFile, 'utf-8'));
+}
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      
+      const environment = config.env.configFile || 'qa';
+      const envConfig = getConfigurationByFile(environment);
+
+      config.baseUrl = envConfig.baseUrl;
+      config.env.apiUrl = envConfig.apiUrl;
+
+      return config;
+    },
+  },
+});
